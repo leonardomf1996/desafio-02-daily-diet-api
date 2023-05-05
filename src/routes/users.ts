390f308f448fb90cb5cd629534fae02d92bcb30b
Leonardo Furtado <leonardomf1996@gmail.com>
2023-05-05 14:59:31 -0300

    feat: create user route

---
+++ src/routes/users.ts
@@ -0,0 +1,53 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method} ${request.url}]`)
+  })
+
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      fullname: z.string(),
+      mail: z.string(),
+      password: z.string(),
+      confirmPassword: z.string(),
+    })
+    const { fullname, mail, password, confirmPassword } =
+      createUserBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 15,
+      })
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (!emailRegex.test(mail)) {
+      throw new Error('Invalid mail')
+    }
+
+    if (password !== confirmPassword) {
+      throw new Error('Invalid password')
+    }
+
+    await knex('users').insert({
+      id: randomUUID(),
+      fullname,
+      mail,
+      password,
+      created_at: new Date(),
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
