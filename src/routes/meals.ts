73ad00f27a7d4c0c493c4bae0d5ee8788efecb83
Leonardo Furtado <leonardomf1996@gmail.com>
2023-05-10 20:38:58 -0300

    feat: get meal and list meal requests

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -153,4 +153,92 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(204).send()
     },
   )
+
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      if (!sessionId) {
+        throw new Error('Unauthorized user')
+      }
+
+      const user = await knex('users').select('*').where({
+        session_id: sessionId,
+      })
+
+      const userId = user[0].id
+
+      if (!userId) {
+        throw new Error('User not found')
+      }
+
+      const findMeals = await knex('meals').select('*').where('user_id', userId)
+
+      if (!findMeals) {
+        throw new Error('Meals not found')
+      }
+
+      const meals = findMeals.map((meal) => {
+        const ingested = new Date(meal.ingested_at)
+
+        return {
+          ...meal,
+          ingested_at: ingested,
+        }
+      })
+
+      return { meals }
+    },
+  )
+
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      if (!sessionId) {
+        throw new Error('Unauthorized user')
+      }
+
+      const user = await knex('users').select('*').where({
+        session_id: sessionId,
+      })
+
+      const userId = user[0].id
+
+      if (!userId) {
+        throw new Error('User not found')
+      }
+
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getMealParamsSchema.parse(request.params)
+
+      const findMeal = await knex('meals').select('*').where({ id })
+
+      if (!findMeal) {
+        throw new Error('Meal not found')
+      }
+
+      const ingested = new Date(findMeal[0].ingested_at)
+
+      delete findMeal[0].ingested_at
+
+      const meal = {
+        ...findMeal[0],
+        ingested_at: ingested,
+      }
+
+      return { meal }
+    },
+  )
 }
