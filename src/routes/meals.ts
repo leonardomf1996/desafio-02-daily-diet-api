5a3b4f909cb8a25868febcd7aafaf96a2993a420
Leonardo Furtado <leonardomf1996@gmail.com>
2023-05-10 20:07:23 -0300

    feat: update meal request

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -53,4 +53,64 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(201).send()
     },
   )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      if (!sessionId) {
+        throw new Error('Unauthorized user')
+      }
+
+      const updateMealParamsSchema = z.object({
+        id: z.string(),
+      })
+
+      const updateMealBodySchema = z.object({
+        mealName: z.string(),
+        description: z.string(),
+        ingestedAt: z.string().optional(),
+        insideDietPlan: z.boolean(),
+      })
+
+      const { id } = updateMealParamsSchema.parse(request.params)
+
+      const { mealName, description, ingestedAt, insideDietPlan } =
+        updateMealBodySchema.parse(request.body)
+
+      const user = await knex('users').select('*').where({
+        session_id: sessionId,
+      })
+
+      const userId = user[0].id
+
+      if (!userId) {
+        throw new Error('User not found')
+      }
+
+      const meal = await knex('meals').select('*').where({ id })
+
+      if (!meal) {
+        throw new Error('Meal not found')
+      }
+
+      await knex('meals')
+        .update({
+          meal_name: mealName,
+          description,
+          ingested_at: ingestedAt ? new Date(ingestedAt) : new Date(),
+          inside_diet_plan: insideDietPlan,
+        })
+        .where({
+          id,
+          user_id: userId,
+        })
+
+      return reply.status(204).send()
+    },
+  )
 }
